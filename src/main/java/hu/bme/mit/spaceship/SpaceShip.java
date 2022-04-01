84b7ddaed30f733a435457df253638e7c6dc6bab
patyulator <79419002+patyulator@users.noreply.github.com>
2022-04-01 13:42:23 +0200

    Update SpaceShip.java

--- src/main/java/hu/bme/mit/spaceship/SpaceShip.java
+++ src/main/java/hu/bme/mit/spaceship/SpaceShip.java
@@ -2,7 +2,7 @@ package hu.bme.mit.spaceship;
 
 /**
 * Defines basic spaceship functionality
-* (collects just the most important ones currently)
+e most important ones currently)
 */
 public interface SpaceShip {
 
